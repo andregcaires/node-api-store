ce8718c3ed786a1bc467c6232e0f2bb6dfe02606
andregcaires <andregcaires@outlook.com>
2020-02-23 18:45:36 -0300

    add validators

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -2,6 +2,7 @@
 
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
+const ValidationContract = require('../validators/fluent-validator')
 
 exports.getAll = (req, res, next) => {
     Product.find({}, 'title slug price tags')
@@ -53,6 +54,17 @@ exports.getBySlug = (req, res, next) => {
 }
 
 exports.post = (req, res, next) => {
+
+    // realiza fluent validations
+    let contract = new ValidationContract();
+    contract.hasMinLength(req.body.title, 3, 'O título deve conter ao menos 3 caracteres');
+    contract.hasMinLength(req.body.slug, 3, 'O slug deve conter ao menos 3 caracteres');
+
+    if(!contract.isValid()) {
+        res.status(400).send(contract.errors()).end();
+        return;
+    }
+
     let product = new Product(req.body);
     product.save()
         .then(x => {
---
+++ src/validators/fluent-validator.js
@@ -0,0 +1,40 @@
+'use strict'
+
+let errors = [];
+
+function ValidationContract() {
+    errors = [];
+}
+
+ValidationContract.prototype.isRequired = (value, message) => {
+    if(!value || value.length == 0)
+        errors.push({ message: message });
+}
+
+ValidationContract.prototype.hasMinLength = (value, min, message) => {
+    if(!value || value.length < min )
+        errors.push({ message: message });
+}
+
+ValidationContract.prototype.hasMaxLength = (value, max, message) => {
+    if(!value || value.length > max )
+        errors.push({ message: message });
+}
+
+ValidationContract.prototype.isFixedLength = (value, length, message) => {
+    if(!value || value.length !== length)
+        errors.push({ message: message });
+}
+
+ValidationContract.prototype.errors = () => {
+    return errors;
+}
+
+ValidationContract.prototype.clear = () => {
+    errors = [];
+}
+ValidationContract.prototype.isValid = () => {
+    return errors.length === 0;
+}
+
+module.exports = ValidationContract;
