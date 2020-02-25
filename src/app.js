12f80f05ac8d401333a1a7291e2f7ff77125732e
andregcaires <andregcaires@outlook.com>
2020-02-25 19:02:13 -0300

    CORS added

--- src/app.js
+++ src/app.js
@@ -26,10 +26,19 @@ const orderRoutes = require('./routes/order-routes');
 
 // registra bodyparser, que age como middleware para realizar
 // conversão do corpo da requisição para json
-app.use(bodyParser.json());
+app.use(bodyParser.json({
+    limit: '5mb'
+}));
 app.use(bodyParser.urlencoded({
     extended: false
 }));
+
+// Habilita o CORS  
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origins', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+});
 
 // registra rotas
 app.use('/', indexRoutes);
